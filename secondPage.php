dc3961d0bddbdcab59b9cc62de1aa39c5da07d4f
JGravesJr <jgravesjr@outlook.com>
2016-07-22 11:45:21 -0400

    Added session and cookie data to secondPage.

--- secondPage.php
+++ secondPage.php
@@ -5,6 +5,32 @@
     <title>Second Page</title>
   </head>
   <body>
+    <?php
+    #Cookies#
+    $name = "test";
+    $value = 15;
+    #Unix time set, php works it in by default. This is a simple
+    #equation to add up to a week. 
+    $expire = time() + (60*60*24*7);
+    setcookie($name, $value, $expire);
+    print_r($_COOKIE);
+    #Checks if test is set before trying to access cookies.
+    $test = isset($_COOKIE["test"]) ? $_COOKIE["test"] : "";
+    echo $test;
+    #Recommended unsetting of a cookie value for logins. 
+    setcookie($name, null, time() - 3600);
+    ?>
+    <?php 
+    #Sessions#
+    #Uses cookie technology to reference data stored on the web
+    #server. Allows for more storage, is pretty slow, expire
+    #when the browser is closed. 
+    session_start();
+    $_SESSION["firstName"] = "J";
+    $name = $_SESSION["firstName"];
+    echo $name;
+    ?>
+
     <!-- A link! -->
     <a href="firstPage.php"> First Page</a>
 
